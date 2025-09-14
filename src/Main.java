7af4909a8a5f3bad8dc87bddd5f5a5e866e78c72
Huthifa  Ahmed Salem Al-fyyad <107212308+Huthifa962@users.noreply.github.com>
2025-09-14 21:49:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+     public class Main {
+    public static void main(String[] args) {
+
+        Printer printerObject;
+        printerObject = new Printer(50,false);
+        System.out.println("Initial page count = " + printerObject.getPagesPrinted());
+
+        int pagesPrinted = printerObject.printPages(5);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n", pagesPrinted,printerObject.getPagesPrinted());
+
+        pagesPrinted = printerObject.printPages(10);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n", pagesPrinted,printerObject.getPagesPrinted());
+    }
+}
+   Printer printerObject;
+        printerObject = new Printer(50,false);
+        System.out.println("Initial page count = " + printerObject.getPagesPrinted());
+
+        int pagesPrinted = printerObject.printPages(5);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n", pagesPrinted,printerObject.getPagesPrinted());
+
+        pagesPrinted = printerObject.printPages(10);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n", pagesPrinted,printerObject.getPagesPrinted());
+    }
 }
